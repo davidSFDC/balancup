a75e35bc141f4a7373a3b4c3947b97b670333c90
davidDeinDeal <david.padilla@deindeal.ch>
2013-05-21 01:05:55 +0200

    test

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -8,7 +8,6 @@ import org.eclipse.jetty.webapp.WebAppContext;
  * This class launches the web application in an embedded Jetty container.
  * This is the entry point to your application. The Java command that is used for
  * launching should fire this main method.
- * Comment to try git
  */
 public class Main {
     

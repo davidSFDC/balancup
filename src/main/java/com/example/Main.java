1025210eb0b339dd0baaba89a967bae6b67cfbcb
davidDeinDeal <david.padilla@deindeal.ch>
2013-05-27 10:45:52 +0200

    Teste

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -16,6 +16,7 @@ public class Main {
      */
     public static void main(String[] args) throws Exception{
         String webappDirLocation = "src/main/webapp/";
+        String a = "a";
         
         //The port that we should run on can be set into an environment variable
         //Look for that variable and default to 8080 if it isn't there.
